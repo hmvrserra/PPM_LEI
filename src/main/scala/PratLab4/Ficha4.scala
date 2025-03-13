c97a4eddaca226457b5bbf75f038915c8b75331e
fx0n <hugomrserra@gmail.com>
2025-03-13 18:49:54 +0000

    Updated 13/3 18:49

--- src/main/scala/PratLab4/Ficha4.scala
+++ src/main/scala/PratLab4/Ficha4.scala
@@ -4,5 +4,10 @@ object Ficha4 extends App {
 
   def sum(xs:List[Int]):Int = (xs foldRight 0)((x: Int, y:Int) => x+y)
   
+  def andFRight(list:List[Boolean]):Boolean =
+    (list foldRight true) ((x:Boolean, y:Boolean) => x||y)
+  
+  
   println(sum(List(1, 2, 3)))
+  println(andFRight(List(false, false, false)))
 }
