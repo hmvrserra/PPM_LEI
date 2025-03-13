8ecd9a39b8ec176e74ff09438897a5490e4336f0
fx0n <hugomrserra@gmail.com>
2025-03-13 19:01:56 +0000

    Updated 13/3 18:%9

--- src/main/scala/PratLab4/Ficha4.scala
+++ src/main/scala/PratLab4/Ficha4.scala
@@ -6,8 +6,14 @@ object Ficha4 extends App {
   
   def andFRight(list:List[Boolean]):Boolean =
     (list foldRight true) ((x:Boolean, y:Boolean) => x||y)
-  
-  
+
+  def remDup[T](list: List[T]): List[T] =
+    list.foldRight(List.empty[T]) {
+      case (elem, acc) if acc.headOption.contains(elem) => acc
+      case (elem, acc) => elem :: acc
+    }
+    
   println(sum(List(1, 2, 3)))
   println(andFRight(List(false, false, false)))
+  print(remDup(List(1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4, 5)))
 }
